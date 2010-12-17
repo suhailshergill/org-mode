aef459a239170464663ede0d5868d43082c5a3f6
Tom Breton (Tehom) <tehom@panix.com>
2010-12-17 15:54:02 -0500

    org-dblock-write:stowed-into disriminates parts of entry.  Not right yet.

--- contrib/lisp/org-stow.el
+++ contrib/lisp/org-stow.el
@@ -34,33 +34,54 @@
 ;;;_. Body
 ;;;_ , org-dblock-write:stowed-into
 (defun org-dblock-write:stowed-into (params)
-   (let* ((id (plist-get params :id))
+   (let* (  (components (org-heading-components))
+	    (m-depth (org-current-level))
+	    (id (plist-get params :id))
 	    ;;$$IMPROVE ME Figure out how deep the block should be.
 	   (text-list
 	      (save-excursion
 		 (org-id-goto id)
+		 (let*
+		    ((subst-depth (org-current-level))
+		       (depth-delta (- subst-depth m-depth 1)))
 		 (org-map-entries
 		    #'(lambda ()
-			 (buffer-substring
-			    (org-entry-beginning-position)
-			    (org-entry-end-position)))
+			 (let
+			    ((prop-drawer-points
+				(org-get-property-block)))
+			    
+			    (concat
+			       ;;Make the headline
+			       (make-string 
+				  (+ (org-current-level) depth-delta)
+				  ?*)
+			       " "
+			       (org-get-heading)
+			       "\n"
+			       ;;Properties block, with any id
+			       ;;transformed.
+			       ;;$$IMPROVE ME - leave this out if
+			       ;;prop-drawer-points is nil.
+			       ;;$$IMPROVE ME - leave out ID lines.
+			       (when prop-drawer-points
+				  (buffer-substring
+				     (car prop-drawer-points)
+				     (cdr prop-drawer-points)))
+			       ;;The rest, to (org-entry-end-position)
+			       
+			       )))
 		    nil
-		    'tree)))
+		    'tree))))
 	    
 	    (inhibit-read-only t))
       ;;$$IMPROVE ME Remove ids from property blocks, replace them
       ;;with a different property that links back to the source.
-      ;;$$IMPROVE ME Change the number of stars in the entry.  Need to
-      ;;know current depth.
-      ;;$$IMPROVE ME Make this read-only.
       ;;$$IMPROVE ME Somehow make todo changes etc affect the source.
       ;;They (correctly) don't affect the read-only text.  Or at least
       ;;put point back there.
       ;;$$IMPROVE ME Somehow make changes in the source propagate back
       ;;- but for now, manually redoing all dynamic blocks will
       ;;suffice. 
-      ;;$$IMPROVE ME Map over entries.  Use org-map-entries with scope
-      ;;`tree'.
       (mapcar 
 	 #'(lambda (text)
 	      (insert (propertize text 'read-only t)))
@@ -68,8 +89,46 @@
 
 ;;;_ , Creating them.
 
-;;`org-create-dblock'
+'
+(org-create-dblock 
+   (list
+      :name "stowed-into" 
+      :id 'source-ID
+      :headline "Headline"))
+
+;;NOT (org-insert-heading), it's too high, does too much.
+
 ;;;_ , Stowing a subtree
+
+;;Find prefix
+;;Find that location (by id or headline)  The id will point at the
+;;  *parent* of our root.  NB, the lower ones can't have real ids
+;;  because they might well live in notes, which shouldn't become our target.
+;;Is it available?  Use it or recurse.  We must recurse thru our prefix.
+;;Recursing, use headlines?  Can't safely use ids, they might point
+;;anywhere. 
+;;Collect what to do.
+;;If there are any conflicts, report them.
+;;Otherwise create dblocks in all the places.  Create our ids if they
+;;don't already exist.
+;;Add a tag "stowed"
+
+;;;_ , Unstowing
+;;Again find all the locations
+;;Erase our dblocks there (We own them or they'd appear as conflicts)
+;;Remove tag "stowed" (back to "stowable")
+
+;;;_ , Mark an item (subtree) stowable
+;;Find its target
+;;Get that id and a prefix.
+;;Store that prefix.
+;;Add a tag "stowable"
+
+;;;_ , Add an item to a stowable subtree
+;;;_ , Integration with org-choose
+
+;;Let choosing (when it moves items to or from a "satisfied" level)
+;;call unstowing and stowing.
 
 ;;;_. Footers
 ;;;_ , Provides
